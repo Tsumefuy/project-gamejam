31d3aa8c886a58af8df5222f46dea478c5585504
Acauã Rangel <71088524+Acaua-Rangel@users.noreply.github.com>
2024-04-19 18:09:48 -0300

    Adicionar diagonais

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,18 +5,20 @@ const speed = 300
 var vel = Vector2()
 
 func get_input():
+	if (not Input.is_action_pressed("ui_up")) and (not Input.is_action_pressed("ui_down")):
+		vel.y = 0
+		
 	if Input.is_action_pressed("ui_up"):
 		vel.y -= 1
-	elif Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):
 		vel.y += 1
-	else:
-		vel.y = 0
+	
 		
 	if Input.is_action_pressed("ui_left"):
 		vel.x -= 1
-	elif Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		vel.x += 1
-	else:
+	if (not Input.is_action_pressed("ui_left")) and (not Input.is_action_pressed("ui_right")):
 		vel.x = 0
 		
 	vel = vel.normalized() * speed
